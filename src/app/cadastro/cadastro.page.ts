9e19cf1cd5b50dc2c35c9559c5767d5e4a83b10a
PEDRO OTAVIO RABELLO LINK <pedro.link@alunos.sc.senac.br>
2025-08-12 16:48:33 -0300

    cadastro acho que feito, nao deu pra testar

---
+++ src/app/cadastro/cadastro.page.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
+import { 
+  IonContent, 
+  IonHeader, 
+  IonTitle, 
+  IonToolbar, 
+  IonCard, 
+  IonCardHeader, 
+  IonCardContent, 
+  IonCardTitle,
+  IonInput,
+  IonInputPasswordToggle,
+  IonButton,
+} from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { Requisicao } from '../service/requisicao';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.page.html',
+  styleUrls: ['./cadastro.page.scss'],
+  standalone: true,
+  imports: [IonInput, 
+    IonContent, 
+    IonHeader, 
+    IonTitle, 
+    IonToolbar, 
+    CommonModule, 
+    FormsModule,  
+    IonCard, 
+    IonCardHeader, 
+    IonCardContent, 
+    IonCardTitle,
+    IonInputPasswordToggle,
+    IonButton,
+    RouterLink
+  ]
+})
+export class CadastroPage implements OnInit {
+  public login:string = "";
+  public nome:string = "";
+  public email:string = "";
+  public senha:string = "";
+
+  constructor(
+    public rs :Requisicao
+  ) { }
+
+  ngOnInit() {
+    
+  }
+
+  post(){
+    const fd = new FormData();
+    fd.append('controller','cadastro-usuario');
+    fd.append('nome', this.nome);
+    fd.append('login', this.login);
+    fd.append('email', this.email);
+    fd.append('senha', this.senha);
+
+    this.rs.post(fd)
+    .subscribe();
+  }
+}
