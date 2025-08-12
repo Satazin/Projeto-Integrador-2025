265a97357c69fac2ac153d83dd006c6f3c3fd8cd
PEDRO OTAVIO RABELLO LINK <pedro.link@alunos.sc.senac.br>
2025-08-12 16:15:01 -0300

    Instalação Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,13 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-HGkUTe_NDtsK015lILyq2-QwDf-s_Mw",
+  authDomain: "projetointegrador-69272.firebaseapp.com",
+  databaseURL: "https://projetointegrador-69272-default-rtdb.firebaseio.com",
+  projectId: "projetointegrador-69272",
+  storageBucket: "projetointegrador-69272.firebasestorage.app",
+  messagingSenderId: "512186215917",
+  appId: "1:512186215917:web:fd6924556a1186274f8be4"
+}
