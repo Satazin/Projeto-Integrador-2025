f4efe8e7fb81a6aef2f03e4d78d427bb143c9e56
DAVIH RIBEIRO SILVEIRA <davih.silveira@alunos.sc.senac.br>
2025-08-13 10:26:13 -0300

    pagina para pedidos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenu, IonMenuButton, IonApp, IonItem, IonList, IonLabel, IonButton, IonFooter, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonRouterOutlet, IonFooter, IonButton, IonLabel, IonList, IonItem, IonApp, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, IonMenu, IonMenuButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
   constructor() {}
