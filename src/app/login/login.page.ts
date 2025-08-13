f645deed6b35358cbd43d11c53f197d603728b67
PEDRO OTAVIO RABELLO LINK <pedro.link@alunos.sc.senac.br>
2025-08-13 09:12:00 -0300

    Tela de Login criada, falta ser testada com firebase

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonImg, IonButton, IonCard } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { Autenticacao } from '../service/autenticacao';
+import { IonicModule } from '@ionic/angular'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
 })
 export class LoginPage implements OnInit {
+  public email: string = '';
+  public senha: string = '';
 
-  constructor() { }
+  constructor(
+    public Autenticacao_Service: Autenticacao,
+  ) { }
 
   ngOnInit() {
   }
+  login() {
+    let email = this.email;
+    let senha = this.senha;
 
+    this.Autenticacao_Service
+      .login(email, senha)
+      .subscribe(
+        (_res: any) => {
+          if (_res.status == 'success') {
+            sessionStorage.setItem('token', _res.token);
+          } else {
+
+          }
+        }
+      );
+  }
 }
