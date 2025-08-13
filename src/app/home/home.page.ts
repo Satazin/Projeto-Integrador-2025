454e19afff26097cefde2ed6f606d514c4d72a95
LAURA SOPHIA HAINZENREDER APOLINARIO <laura.apolinario@alunos.sc.senac.br>
2025-08-13 11:53:22 -0300

    css cadastro

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonicModule],
+  imports: [IonicModule, RouterLink],
 })
 export class HomePage {
   constructor() {}
