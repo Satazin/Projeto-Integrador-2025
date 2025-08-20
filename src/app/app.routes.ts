100d67c5615c9d8c56c3be3aad487f6d227ece12
DAVIH RIBEIRO SILVEIRA <davih.silveira@alunos.sc.senac.br>
2025-08-20 08:58:34 -0300

    pagina de pedidos quase feita

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -26,7 +26,11 @@ export const routes: Routes = [
   {
     path: 'forgot-password',
     loadComponent: () => import('./forgot-password/forgot-password.page').then( m => m.ForgotPasswordPage)
+  },  {
+    path: 'carrinho',
+    loadComponent: () => import('./carrinho/carrinho.page').then( m => m.CarrinhoPage)
   },
+
 
 
 ];
--- src/app/pedidos/pedidos.page.ts
+++ src/app/pedidos/pedidos.page.ts
@@ -1,18 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { IonContent, 
+  IonHeader, 
+  IonTitle, 
+  IonToolbar, 
+  IonMenu, 
+  IonIcon, 
+  IonButton,
+  IonButtons, 
+  IonMenuButton, 
+  IonList, 
+  IonAvatar 
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-pedidos',
   templateUrl: './pedidos.page.html',
   styleUrls: ['./pedidos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonAvatar,
+    IonList,
+    IonButtons,
+    IonButton, 
+    IonIcon, 
+    IonContent, 
+    IonHeader, 
+    IonTitle, 
+    IonToolbar, 
+    CommonModule,
+    FormsModule, 
+    IonMenu, 
+    IonMenuButton, 
+    RouterLink
+  ]
 })
 export class PedidosPage implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
